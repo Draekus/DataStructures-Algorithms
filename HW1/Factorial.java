af082f5e1331b33c848f1ff9cf9f7f68b6420fc5
Patrick Gould <42391793+Draekus@users.noreply.github.com>
2023-01-25 18:12:44 +0000

    Started fixing BigInteger overflow issue.

--- HW1/Factorial.java
+++ HW1/Factorial.java
@@ -20,18 +20,19 @@ public class Factorial {
             // If the user enters q,Q,quit the program will exit and notify the user
             if (user_input.equals("q") || user_input.equals("Q") || user_input.equals("quit")) {
                 exit = true;
-                System.out.println("Program exited cleanly.");
             }
             else {
                 // Cast the string from the text buffer to an int
                 user_int = Integer.parseInt(user_input);
                 // Call factorial on the user's value
                 System.out.print("" + user_int + "! is equal to " + factorial(user_int) + "\n\n");
+         
             }
             
         }
         // Close scanner
         scan.close();
+        System.out.println("Program exited cleanly.");
     }
 
     // Factorial function 
